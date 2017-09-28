ce769f5ac26260aaf3232bac127b2ee2cd1a29ee
ij2872 <ijaimes2872@gmail.com>
2017-09-28 17:08:57 -0500

    Revert Changes not Created

--- MyHabbits/App_Start/FilterConfig.cs
+++ MyHabbits/App_Start/FilterConfig.cs
@@ -8,7 +8,6 @@ namespace MyHabbits
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
-            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
--- MyHabbits/Startup.cs
+++ MyHabbits/Startup.cs
@@ -9,6 +9,7 @@ namespace MyHabbits
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+
         }
     }
 }
