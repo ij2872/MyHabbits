6c58c1bd3c0c01fe19b6fa721cd4143ba2d3d614
ij2872 <ijaimes2872@gmail.com>
2017-09-28 16:30:21 -0500

    Add Elmah installed/Https Enabled

--- MyHabbits/App_Start/FilterConfig.cs
+++ MyHabbits/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace MyHabbits
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
